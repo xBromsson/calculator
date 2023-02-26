f31e613f967b9b6ca955284d02180d9df7c7ee0a
xBromsson <marvin@the-firebrand.com>
2023-02-26 17:16:41 -0600

    basic operator functions, and html div elements

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+
+
+
+function operate(operater, a, b);
+
+    if (operater === "+"){
+        function add(){
+            result = a + b
+            return result
+        }
+    }
+
+    if (operater === "-"){
+        function subtract(){
+            result = a - b
+            return result
+        }
+    }
+
+    if (operater === "*"){
+        function multiply(){
+            result = a * b
+            return result
+        }
+    }
+
+    if (operater === "/"){
+        function divide(){
+            result = a / b
+            return result
+        }
+    }
+
+
+
