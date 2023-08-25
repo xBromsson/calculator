497cab6e6fa72d3ce1dedf26070b13e213faf4e9
Marvin Brown <marvin@the-firebrand.com>
2023-08-25 15:13:01 -0500

    finished handling some edge cases and formatting numbers

--- javascript.js
+++ javascript.js
@@ -1,130 +1,122 @@
-let displayValue = document.querySelector('#display');
-let subDisplayValue = document.querySelector('#subdisplay');
+let displayValue = document.querySelector("#display");
+let subDisplayValue = document.querySelector("#subdisplay");
 let firstNumber = "";
 let secondNumber = "";
 let result = "";
 let operator = "";
-let process = false;
 
-const numberArray = document.querySelectorAll('.number');
-const actionArray = document.querySelectorAll('.action');
-const operationArray = document.querySelectorAll('.operation');
+const numberArray = document.querySelectorAll(".number");
+const actionArray = document.querySelectorAll(".action");
+const operationArray = document.querySelectorAll(".operation");
 
-
-//this just updates the display to match the innards
-function updateDisplay(){
-    
-    if (operator === ""){
-        displayValue.textContent = firstNumber.toString();
-        if (firstNumber === ""){displayValue.textContent = "0"}
-    } else if (operator != ""){
-        displayValue.textContent = secondNumber.toString();
-        if (secondNumber === ""){displayValue.textContent = "0"}
+function updateDisplay() {
+  if (!operator) {
+    displayValue.textContent = firstNumber;
+    if (!firstNumber) {
+      displayValue.textContent = "0";
     }
-    
-
+  } else if (operator) {
+    displayValue.textContent = secondNumber;
+    if (!secondNumber) {
+      displayValue.textContent = "0";
+    }
+  }
 }
 
-function updateSubDisplay(){
-    subDisplayValue.textContent = firstNumber + operator;
+function limitInput(input) {
+  return input.substring(0, 8);
 }
 
-//listens for events on the number buttons 1 through 9 
-for(let i = 0; i < numberArray.length; i++){
-    let self = numberArray[i]
-    self.addEventListener('click', (event) => {
-
-        if (result != ""){
-            firstNumber = "";
-            secondNumber = "";
-            operator = "";
-            result = "";
-        }
-
-        if (operator === ""){
-            firstNumber += self.textContent;
-        } else if (operator != ""){
-            secondNumber += self.textContent;
-        } 
-
-        updateDisplay();    
-    });    
-}
-
-//listens for events on the operations button , add, sub, div, multiply
-for(let i = 0; i < operationArray.length; i++){
-    let self = operationArray[i]
-    self.addEventListener('click', (event) => {
-
-        operator = self.textContent;
-        if (result != ""){
-            secondNumber = "";
-            result = "";
-        }
-    
-    });    
-}
-
-//listens for events on the action buttons, equal, make negative, clear, 
-for(let i = 0; i < actionArray.length; i++){
-    let self = actionArray[i]
-    self.addEventListener('click', (event) => {
-
-        if (self.id === "ac"){
-            firstNumber = "";
-            secondNumber = "";
-            result = "";
-            operator = "";
-            process = false;
-            updateDisplay();
-            //updateSubDisplay();
-        }
-
-        if (self.id === "operate"){
-            result = operate(operator, parseInt(firstNumber), parseInt(secondNumber));
-            displayValue.textContent = result;
-            firstNumber = result;
-        }
-    
-        if (self.id === "swap"){
-            if (operator === ""){
-                firstNumber -= (parseInt(firstNumber) * 2);
-            } else if (result != ""){
-                firstNumber -= (firstNumber * 2);
-            } else if (operator != ""){
-                secondNumber -= (secondNumber * 2);
-            } 
-
-            updateDisplay;
-        }
-    });    
-}
-
-// controls the math operations of the calculator
-function operate(operator, a, b){
-    let result = 0;
-
-    if (operator === "+"){
-        result = a + b
+for (let i = 0; i < numberArray.length; i++) {
+  let clickedNumber = numberArray[i];
+  clickedNumber.addEventListener("click", (event) => {
+    if (result) {
+      firstNumber = "";
+      secondNumber = "";
+      operator = "";
+      result = "";
     }
 
-    if (operator === "-"){
-            result = a - b
+    if (clickedNumber.textContent === ".") {
+      if (firstNumber.includes(".") || secondNumber.includes(".")) {
+        return;
+      }
     }
 
-    if (operator === "*"){
-            result = a * b
+    if (!operator) {
+      firstNumber = limitInput(firstNumber + clickedNumber.textContent);
+    } else {
+      secondNumber = limitInput(secondNumber + clickedNumber.textContent);
     }
 
-    if (operator === "/"){
-            result = a / b
-    }
-    return result.toString();
-
+    updateDisplay();
+  });
 }
 
+for (let i = 0; i < operationArray.length; i++) {
+  let self = operationArray[i];
+  self.addEventListener("click", (event) => {
+    operator = self.textContent;
+    if (result) {
+      secondNumber = "";
+      result = "";
+    }
+  });
+}
 
+for (let i = 0; i < actionArray.length; i++) {
+  let self = actionArray[i];
+  self.addEventListener("click", (event) => {
+    if (self.id === "ac") {
+      firstNumber = "";
+      secondNumber = "";
+      result = "";
+      operator = "";
+      updateDisplay();
+    }
 
+    if (self.id === "operate") {
+      result = operate(operator, Number(firstNumber), Number(secondNumber));
+      displayValue.textContent = result;
+      firstNumber = result;
+    }
 
+    if (self.id === "swap") {
+      if (!operator) {
+        firstNumber = limitInput(String(-Number(firstNumber)));
+      } else if (result) {
+        firstNumber = limitInput(String(-Number(firstNumber)));
+      } else if (operator) {
+        secondNumber = limitInput(String(-Number(secondNumber)));
+      }
 
+      updateDisplay();
+    }
+  });
+}
 
+function operate(operator, a, b) {
+  let calcResult = 0;
+
+  if (operator === "+") {
+    calcResult = a + b;
+  }
+
+  if (operator === "-") {
+    calcResult = a - b;
+  }
+
+  if (operator === "*") {
+    calcResult = a * b;
+  }
+
+  if (operator === "/") {
+    calcResult = a / b;
+  }
+
+  // Round the result to 4 decimal places
+  calcResult = Math.round(calcResult * 10000) / 10000;
+
+  result = calcResult.toString();
+  return result;
+}
